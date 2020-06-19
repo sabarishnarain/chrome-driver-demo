34ec99d22feb25ad0f5abff042d91a8dad9a0100
sabarishnarain <sabarish.narain@gmail.com>
2020-06-19 11:13:32 -0500

    windows support

--- launch.js
+++ launch.js
@@ -2,7 +2,11 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
  const path = require('path');
 
 (async function example() {
-  process.env['PATH'] = process.env.PATH + ":" + path.join('node_modules','.bin');
+  if (process.platform === 'win32') {
+    process.env['PATH'] = process.env.PATH + ";" + path.resolve('./node_modules','.bin');
+  } else {
+    process.env['PATH'] = process.env.PATH + ":" + path.resolve('./node_modules','.bin');
+  }
   let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://www.expense.familynetworx.com');
